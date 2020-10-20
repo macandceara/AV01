e161d23865927fe0c7992913fbf5d598b54e4e88
Daniel Macedo <70235487+macandceara@users.noreply.github.com>
2020-10-20 16:42:06 -0300

    Update mdc.js

--- mdc.js
+++ mdc.js
@@ -1,15 +1,23 @@
-//Máximo divisor comum: O máximo divisor comum (mdc) de dois inteiros a, b é o maior número inteiro que divide a e b.
-var a = 10; // primeiro numero
-var b = 20; //segundo numero
-var i;     // for
-function mdc(a, b){
-    for (i = 0; )
+//Máximo divisor comum: O máximo divisor comum (mdc) de dois inteiros a, b é o maior número inteiro que divide a e b.//Máximo divisor comum: O máximo divisor comum (mdc) de dois inteiros a, b é o maior número inteiro que divide a e b.
+var a = 8; // primeiro numero
+var b = 6; //segundo numero
+var divisor = 2;     
+var mdc = 1;
 
-    while (a % candidato != 0 || b % candidato != 0){
-           candidato--;
+
+    while (divisor <= a && divisor <= b) {
+      
+         if (a % divisor == 0 && b  % divisor == 0) {
+         mdc *= divisor;
+          a /= divisor;
+          b /= divisor;
+         divisor = 2;
+        }
+      
+    else {
+      divisor++;  
     } 
 
-    return candidato;
 }
 
-console.log(" O mdc dos numeros " +a+ " e " +b+ " é " + mdc(a,b))
+console.log(" O mdc dos numeros informados é " + mdc);
